9afe0bd88138fdb4e79808b8c4475eb67ed7a4c8
Rayden97 <sonicrage8@yandex.ru>
2024-01-19 22:27:14 +0300

    posle review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", 'default_key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
